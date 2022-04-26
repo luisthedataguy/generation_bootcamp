1d0009663a3753279921513aba3e17fc2c859c83
LuisG <luisillo7@comunidad.unam.mx>
2022-04-26 14:43:46 -0500

    mensaje

---
+++ javascript/tiposdedatos/assets/js/ejercicio.js
@@ -0,0 +1,11 @@
+nombre = prompt("¿Cual es tu nombre?");
+edad = prompt("En que año naciste?");
+
+// nombre = prompt("¿Cual es tu nombre?");
+
+// edad = document.getElementById("#input");
+// {
+// }
+
+const edadactual = 2022 - parseInt(edad);
+console.log(`Hola ${nombre}, tu edad es ${edadactual}`);
---
+++ javascript/tiposdedatos/assets/js/main.js
@@ -0,0 +1,25 @@
+let nombre = "Jonathan";
+let edad = 23;
+const nombreCompleto = "Luis Manuel Garcia";
+let tinesTrabajo = true;
+let variableNull = null;
+
+let variableIndefinida;
+
+console.log(typeof nombre);
+console.log(typeof nombreCompleto);
+console.log(typeof edad);
+console.log(typeof tinesTrabajo);
+console.log(typeof variableNull);
+console.log(typeof variableIndefinida);
+
+if (true) {
+  let numero = 10;
+  console.log(numero);
+}
+
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
+
+console.log(i);
